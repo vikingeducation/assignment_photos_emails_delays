76fa668f0c05f2a19aef78aeb2ae6e1bc8875ede
Michael Alexander <alexandermw@gmail.com>
2014-10-27 21:32:03 -0700

    generate migrations for paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_attached_file :profile, :styles => { :medium => "300x300", :thumb => "100x100" }
+
+  validates_attachment_content_type :profile, :content_type => /\Aimage\/.*\Z/
+
 end
