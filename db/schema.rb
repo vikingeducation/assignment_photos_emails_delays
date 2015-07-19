a4ed2ad95ddcf1705520f37204ad07eaf300ca38
niconan <nick.hax@gmail.com>
2015-07-18 18:12:31 -0700

    add delayed jobs

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20150717023144) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.binary   "profile_photo"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "profile_photo_file_name"
