2ec0e747780b3d75dbaac50b8104408690765710
niconan <nick.hax@gmail.com>
2015-07-18 17:33:15 -0700

    maybe?

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: 'app38880671@heroku.com',
+    password: 'rekohaln0204',
+    domain: "https://quiet-spire-6855.herokuapp.com/",
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: 'localhost' }
