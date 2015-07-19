b4a09ce2b980f52b7d2c8c67a31f776af1cb9739
niconan <nick.hax@gmail.com>
2015-07-18 17:44:46 -0700

    fix app url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.default_url_options = { host: "https://quiet-spire-6855.herokuapp.com" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
