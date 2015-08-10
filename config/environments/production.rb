54d79dca3013d0616f4f20aa118d9a617df6de19
Joseph Lozano <lozanojoseph@gmail.com>
2015-08-10 15:23:08 -0700

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,21 @@
 Rails.application.configure do
+  Paperclip.options[:command_path] = "/usr/local/bin"
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      # put your host name here if needed
+      # see the reading below for more details
+      :s3_host_name => "s3-us-west-2.amazonaws.com",
+      # NOTE: these lines are changed to use secrets.yml
+      # from the examples (which use ENV vars instead)
+      :bucket => Rails.application.secrets.s3_bucket_name,
+      :access_key_id => Rails.application.secrets.aws_access_key_id,
+      :secret_access_key => Rails.application.secrets.aws_secret_access_key
+    }
+  }
+
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -66,7 +83,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
