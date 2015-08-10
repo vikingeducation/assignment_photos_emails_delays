d3470d2dbc12535f52b3c4861de8c8718a131d6e
David <davidmeza@outlook.com>
2015-08-10 16:59:06 -0400

    file upload working with aws

--- photo_emails_delays/config/environments/development.rb
+++ photo_emails_delays/config/environments/development.rb
@@ -39,15 +39,15 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  Paperclip.options[:command_path] = "/usr/local/bin"
+  # Paperclip.options[:command_path] = "/usr/local/bin"
 
   config.paperclip_defaults = {
+    storage: :s3,
+    s3_host_name: 's3.amazonaws.com',
     :s3_credentials => {
-      :secret_access_key => Rails.application.secrets.aws_secret_access_key,
-      :s3_bucket_name => Rails.application.secrets.
-        s3_bucket_name,
-      :aws_access_key_id => Rails.application.secrets.aws_access_key_id
-
+      :secret_access_key => Rails.application.secrets.secret_access_key,
+      :bucket => Rails.application.secrets.s3_bucket_name,
+      :access_key_id => Rails.application.secrets.aws_access_key_id
     }
   }
 
