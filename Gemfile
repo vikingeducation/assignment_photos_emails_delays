69ff39de325c275fca83f109c98ecbc67e314811
facingsouth <xin.d.liu@gmail.com>
2015-08-10 14:52:43 -0700

    setup figaro s3

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Paperclip for file uploads
 gem 'paperclip'
 
+gem "figaro"
+gem 'aws-sdk'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,20 @@ Rails.application.configure do
 
   Paperclip.options[:command_path] = "/usr/local/bin"
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      # put your host name here if needed
+      # see the reading below for more details
+      :s3_host_name => "s3-website-us-west-2.amazonaws.com",
+      # NOTE: these lines are changed to use secrets.yml
+      # from the examples (which use ENV vars instead)
+      :bucket => Rails.application.secrets.s3_bucket_name,
+      :access_key_id => Rails.application.secrets.aws_access_key_id,
+      :secret_access_key => Rails.application.secrets.aws_secret_access_key
+    }
+  }
+
   # Do not eager load code on boot.
   config.eager_load = false
 
