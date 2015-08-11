3a88edb67b59bd875f4101820c1ad0923b1cfded
David <davidmeza@outlook.com>
2015-08-11 12:26:46 -0400

    solve connection refused problem

--- photo_emails_delays/config/environments/development.rb
+++ photo_emails_delays/config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.delivery_method = :letter_opener
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations
