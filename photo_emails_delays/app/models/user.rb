efd4ae0e90bdbef200b01ae70900c41485b03a74
David <davidmeza@outlook.com>
2015-08-11 12:22:50 -0400

    add mailer

--- photo_emails_delays/app/models/user.rb
+++ photo_emails_delays/app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => {:medium => "300x300", :thumb => "100x100"}
   validates_attachment_content_type :avatar ,
                                    :content_type => /\Aimage\/.*\Z/
+
+  after_create :send_welcome_email
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver!
+  end
 end
