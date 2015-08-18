fb220336e36ae70548463e67ef9185704a70c457
Adam <adam.kinson@gmail.com>
2015-08-18 12:03:09 -0400

    complete Paperclip S3 integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
 
   has_attached_file :profile_photo, :styles => { :medium => "300x300", :thumb => "100x100" }
   validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :profile_photo, :less_than => 2.megabytes
 
 end
