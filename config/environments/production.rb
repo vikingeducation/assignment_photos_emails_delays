b8330a619c9131dfe3b38b04323dc57f261aa5da
Bideo Wego <bideowego@gmail.com>
2015-12-06 18:59:12 -0500

    Update production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,15 @@ Rails.application.configure do
   # ----------------------------------------
   # Letter Opener
   # ----------------------------------------
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
   config.action_mailer.delivery_method ||= :smtp
   config.action_mailer.default_url_options = {:host => 'www.bideowego-photos-emails-delays.herokuapp.com'}
 
