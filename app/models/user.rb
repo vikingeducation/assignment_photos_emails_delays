0817af323b1306e095038f4a6a1df27c3cf47922
Justin Mullis <justin.mullis@gmail.com>
2015-12-22 19:50:36 -0500

    change paperclip style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_attached_file :photo, styles: { medium: ["150", :png] }
+  has_attached_file :photo, styles: { medium: ["600", :png] }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
   validates_with AttachmentPresenceValidator, attributes: :photo
 
