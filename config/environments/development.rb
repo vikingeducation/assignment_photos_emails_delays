8a28cac3740b3904d5013c388392f70af0c836f9
Andrew Baik <and.baik@gmail.com>
2016-02-22 14:31:51 -0500

    some config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
     storage: :s3,
 
     s3_credentials: {
-      s3_host_name: "s3-us-east-1.amazonaws.com",
+      url: "s3-us-east-1.amazonaws.com",
       bucket: Rails.application.secrets.s3_bucket_name,
       access_key_id: Rails.application.secrets.aws_access_key_id,
       secret_access_key: Rails.application.secrets.aws_secret_access_key
