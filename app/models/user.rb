5dedfa9ae8f78fff24fc8914adb0dd52879ea1ae
Julia Herron <julia.herron@gmail.com>
2016-02-22 11:12:00 -0800

    source file options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
 
-  has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100" }
+  has_attached_file :avatar, 
+            :source_file_options => {all: '-rotate 90'},
+            :styles => { :medium => "300x300", :thumb => "100x100" }
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 
   attr_accessor :delete_avatar
   before_validation { avatar.clear if delete_avatar == '1' }
-  
+
 
   def profile_photo=(profile_photo)
     self.profile_photo_data = profile_photo.read
