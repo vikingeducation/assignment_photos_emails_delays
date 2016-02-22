e431d9c01b734559b9477fa970907490719cd163
Julia Herron <julia.herron@gmail.com>
2016-02-22 11:50:59 -0800

    missing bracket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,6 @@ Rails.application.configure do
     :bucket => Rails.application.secrets.s3_bucket_name,
     :access_key_id => Rails.application.secrets.aws_access_key_id,
     :secret_access_key => Rails.application.secrets.aws_secret_access_key
+    }
   }
 end
