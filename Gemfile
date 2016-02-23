efe519106df368b030a314661fea0e50191e93de
koziscool <kozjob@yahoo.com>
2016-02-23 12:06:29 -0600

    delayed job works

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.5.1'
 # Use sqlite3 as the database for Active Record
 
+gem 'delayed_job_active_record'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,14 +25,13 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      User.delay.send_welcome_email(@user.id)
+      flash[:success] = "successfully created"
+      redirect_to @user
+    else
+      flash[:error] = "not successfully created"
+      render :new
     end
   end
 
@@ -76,4 +75,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :email, :avatar)
     end
+
 end
