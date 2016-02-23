0b02f700d0473e11d0bbf53060ae28de4eb7a73e
koziscool <kozjob@yahoo.com>
2016-02-23 17:00:10 -0600

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,16 +89,16 @@ Rails.application.configure do
   }
 }
 
-  # config.action_mailer.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => Rails.application.secrets.sendgrid_username,
-  #   :password       => Rails.application.secrets.sendgrid_email,
-  #   :domain         => 'heroku.com',
-  #   :enable_starttls_auto => true
-  # }
-  # config.action_mailer.delivery_method ||= :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => Rails.application.secrets.sendgrid_username,
+    :password       => Rails.application.secrets.sendgrid_email,
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method ||= :smtp
 
   
 end
