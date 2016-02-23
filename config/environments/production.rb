b1022d14dc66fe8007549b68bab7d5bb97857e52
koziscool <kozjob@yahoo.com>
2016-02-23 17:21:06 -0600

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,12 +93,13 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => Rails.application.secrets.sendgrid_username,
+    :password       => Rails.application.secrets.sendgrid_email,
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method ||= :smtp
+
 
   
 end
