325ff9b63c06ef75be4ca1c4cd8ae9eb205c2b60
Deepa Kackar <dkackar99@yahoo.com>
2016-02-23 10:05:15 -0800

    resolved syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
@@ -113,6 +108,7 @@ Rails.application.configure do
   # Rails also needs to know where your app is
   #   located to properly configure sending of emails
   config.action_mailer.default_url_options = {
-    :host => 'https://hidden-ocean-34841.herokuapp.com/',
+    :host => 'https://hidden-ocean-34841.herokuapp.com/'
   }
+
 end
