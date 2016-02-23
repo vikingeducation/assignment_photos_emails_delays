b3eaee4172db6f28c5f728bac9656b1e4f1e3e07
koziscool <kozjob@yahoo.com>
2016-02-23 16:33:27 -0600

    stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method ||= :smtp
-  config.action_mailer.default_url_options = {
-    :host => 'localhost:300',
+
   }
   
 end
