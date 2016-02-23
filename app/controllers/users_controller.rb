ca90b6c062524abaefc1a7bdfc796fcbaf2a6e4f
Andrew Baik <and.baik@gmail.com>
2016-02-23 11:57:03 -0500

    welcome email upon user creation works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
   # GET /users
   # GET /users.json
   def index
@@ -80,8 +79,8 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(
-        :username, 
-        :email, 
+        :username,
+        :email,
         :profile_photo,
         :avatar,
         :delete_avatar)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
 
-  has_attached_file :avatar, 
+  has_attached_file :avatar,
             :source_file_options => {all: '-rotate 90'},
             :styles => { :medium => "300x300", :thumb => "100x100" }
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
+  after_create :send_welcome_email
 
   attr_accessor :delete_avatar
   before_validation { avatar.clear if delete_avatar == '1' }
@@ -15,6 +15,10 @@ class User < ActiveRecord::Base
     self.profile_photo_data = profile_photo.read
     self.profile_photo_file_name  = profile_photo.original_filename
     self.profile_photo_mime_type = profile_photo.content_type
+  end
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver!
   end
 
 end
