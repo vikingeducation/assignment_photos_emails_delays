3e530852a1338c6f31bcbfc780fcabbc5aae80a2
Steven Chang <prime_pork@hotmail.com>
2016-06-02 19:54:35 +1000

    when a user model is created it calls a message to send a welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,20 @@ class User < ActiveRecord::Base
   # content types to avoid attacks
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+  after_create :send_welcome_email
+
   # For saving profile photo file information into our database.
   # No need for this if not saving profile photo into our own database.
   def photo_data=(photo_data)
     self.data = photo_data.read
     self.filename = photo_data.original_filename
     self.mime_type = photo_data.content_type
+  end
+
+  def send_welcome_email
+    # Note that the bang (!) method will blow up
+    # (roll back) the save transaction on failure. 
+    UserMailer.welcome(self).deliver!
   end
 
 end
