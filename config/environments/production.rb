5e10b3ffdee0e5140054098946daa99d74c00a7a
JeremyVe <jepschx@gmail.com>
2016-06-29 14:34:31 +0800

    changed production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,16 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+          :address        => 'smtp.sendgrid.net',
+          :port           => '587',
+          :authentication => :plain,
+          :user_name      => ENV['SENDGRID_USERNAME'],
+          :password       => ENV['SENDGRID_PASSWORD'],
+          :domain         => 'heroku.com'
+  }
   config.action_mailer.default_url_options = { :host => 'https://peaceful-dawn-72140.herokuapp.com/' }
 
   # Prepend all log lines with the following tags.
