867d135bc4016b53e84905502fdaa2abe6adf635
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-08-18 11:46:05 -0700

    Configuring AWS

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'paperclip'
 gem 'figaro'
+gem 'aws-sdk', '< 2.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100" }
+  has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100" }, :storage => :s3
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  # def s3_credentials
+  #   {:bucket => "#{Rails.application.secrets.BUCKET}", :access_key_id => "#{Rails.application.secrets.ACCESS_KEY_ID}", :secret_access_key => "#{Rails.application.secrets.SECRET_ACCESS_KEY}"}
+  # end
 end
