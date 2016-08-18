8f2665a16c6969b424c793441e75345b16c595ac
cjvirtucio87 <cjv287@gmail.com>
2016-08-18 10:51:37 -0700

    Finished paperclip.

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818162005) do
+ActiveRecord::Schema.define(version: 20160818174530) do
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.binary   "profile_photo", limit: 10485760
+    t.binary   "profile_photo",       limit: 10485760
     t.string   "filename"
     t.string   "mimetype"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
