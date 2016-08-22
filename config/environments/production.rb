874b17e98c808b80cddfd9b6b6676b4e2a487c79
Mike Lee <asackofwheat@gmail.com>
2016-08-22 11:56:28 -0400

    sendgrid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,23 @@ Rails.application.configure do
     :secret_access_key => Rails.application.secrets.aws_secret_access_key
     }
   }
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method ||= :smtp
+
+  # Rails also needs to know where your app is
+  #   located to properly configure sending of emails
+  config.action_mailer.default_url_options = {
+    :host => 'limitless-scrubland-61090.herokuapp.com',
+  }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
