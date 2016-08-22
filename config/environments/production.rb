af738dddc8a62bedccd7dfc4366521c16fe640f5
Adrian Mui <adrian.mui@gmail.com>
2016-08-22 12:55:58 -0400

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,19 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   #config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { :host => 'yourapp.com'}
+
+  config.action_mailer.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method ||= :smtp
+
+  config.action_mailer.default_url_options = { :host => 'warm-wildwood-39856.herokuapp.com'}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
