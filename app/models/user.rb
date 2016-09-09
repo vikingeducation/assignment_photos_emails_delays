85e49d195f412f73f054d94e2a4722fea498207f
schoetlr <srlrs20020@gmail.com>
2016-09-09 10:45:58 -0400

    delayed welcome emails complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  
+
   has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
@@ -9,5 +11,13 @@ class User < ActiveRecord::Base
     self.data = photo.read
     self.filename = photo.original_filename
     self.mime_type = photo.content_type
+  end
+
+
+  private
+
+
+  def self.send_welcome_email(user)
+    UserMailer.delay.welcome(user).deliver!
   end
 end
