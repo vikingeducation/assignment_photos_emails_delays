8818819682d4bcab9d8dbe4d640cdd7cefdbf7f4
Deepak <grc.deepak@gmail.com>
2016-10-18 20:05:12 -0700

    ready for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161019022246) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
