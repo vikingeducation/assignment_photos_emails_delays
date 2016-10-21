11323619eb2d8b09f8ad2dde8238870f5ff0b687
Deepak <grc.deepak@gmail.com>
2016-10-20 20:25:30 -0700

    more stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,17 @@ Rails.application.configure do
     }
   }
 
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method ||= :smtp
+
   Rails.application.routes.default_url_options[:host] = 'sheltered-peak-84152.herokuapp.com'
 
   # Code is not reloaded between requests.
