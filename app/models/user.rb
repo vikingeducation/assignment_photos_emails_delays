6b6112e9b35ea39603b6b4181260c3cb6d6c1c08
James Harris <computersherpa@gmail.com>
2016-12-16 11:43:52 -0600

    Delayed send emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
 
   has_attached_file :photo
   validates_attachment_content_type :photo, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-  
+
+  # private
+    def self.send_welcome_email(id)
+      user = User.find(id)
+      UserMailer.welcome(user).deliver
+    end
+    # handle_asynchronously :send_welcome_email
 
   # def photo=(photo)
   #   self.photo_data = photo.read
