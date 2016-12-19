b6a7c0710a8898abab89c0000fb407e6ccf53b59
Jonathan Popenuck <john.popenuck@gmail.com>
2016-12-19 08:51:05 -0800

    finished

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < ApplicationMailer
-
   default from: "something@example.com"
 
   def welcome(user)
     @user = user
     mail(to: @user.email, subject: "Welcome to this app.")
   end
-  
+
 end
