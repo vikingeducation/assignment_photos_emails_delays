ffae3d5143db6dd2728d7b61a7ec7c1ff638fc74
Jonathan Popenuck <john.popenuck@gmail.com>
2016-12-19 09:44:41 -0800

    did something

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users do
     get "serve"
   end
+  root 'users#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161216170244) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
@@ -24,15 +27,15 @@ ActiveRecord::Schema.define(version: 20161216170244) do
     t.string   "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.binary   "photo_data",         limit: 1048576
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.binary   "photo_data"
     t.string   "photo_filename"
     t.string   "photo_mimetype"
     t.string   "photo_file_path"
