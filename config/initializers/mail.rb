ec17e10f2eed2e573b6f25954eb39a0538870752
James Harris <computersherpa@gmail.com>
2016-12-19 11:42:57 -0600

    Tweak mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,10 @@
-ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
+# ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
 
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '25',
-  :domain => 'https://ancient-castle-22348.herokuapp.com',
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true,
   :authentication => :plain,
   :user_name => Rails.application.secrets.sendgrid_username,
   :password => Rails.application.secrets.sendgrid_password
