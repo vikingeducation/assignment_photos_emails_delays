c52f9434c7114176e5d027c9b2812dab43c87ae1
efraimmgon <efraimmaugusto@gmail.com>
2017-07-03 06:59:51 -0400

    update for production env for sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   ## open with letter_opener
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'photos-emails-delays.herokuapp.com' }
 
+  config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+    }
+  config.action_mailer.delivery_method ||= :smtp
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
