62c9a125dd4fc5be76e9f7aad478f221b276053a
Visiona <darbis@gmail.com>
2017-07-30 18:07:13 +0100

    added paperclip/imagemagick styles and validations for photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-  
-  has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100" }
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  has_attached_file :avatar,
+                    :styles => { :medium => "300x300", :thumb => "100x100" },
+                    :convert_options => {
+                      :thumb => '-set -channel RGBA  -blur 0x8'
+                    }
+  validates_attachment_content_type :avatar,
+                                  :presence => true,
+                                  :size => { :in => 0..10.megabytes },
+                                  :content_type => { :content_type => /^image\/(jpeg|png|gif|tiff)$/
+                                                    }
 end
