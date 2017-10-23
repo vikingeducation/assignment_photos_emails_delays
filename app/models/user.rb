769c2b5022d553796565cc9169113806518724d1
Roy Chen <roychenlei@gmail.com>
2017-10-23 17:54:46 +0800

    change photo storage to local filesystem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # virtual attributes for User photo
-  def user_photo=(user_photo)
-    self.photo_data = user_photo.read
-    self.photo_filename = user_photo.original_filename
-    self.photo_mime_type = user_photo.content_type
-  end
+  # (disabling to store photo uploads in local filesystem instead of DB)
+  
+  # def user_photo=(user_photo)
+  #   self.photo_data = user_photo.read
+  #   self.photo_filename = user_photo.original_filename
+  #   self.photo_mime_type = user_photo.content_type
+  # end
 end
