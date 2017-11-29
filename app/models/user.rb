3f944047f0b929371274d0e6409c030ff8c80668
Roy Chen <roychenlei@gmail.com>
2017-11-29 13:09:29 +0800

    configure delayed sending of welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-  after_create :send_welcome_email
-
   has_attached_file :profile_photo, styles: { medium: "300x300", thumb: "100x100" }
 
   validates_with AttachmentContentTypeValidator, attributes: :profile_photo, content_type: /\Aimage\/.*\Z/
 
   private
 
-  def send_welcome_email
-    UserMailer.welcome(self).deliver!
+  def self.send_welcome_email(id)
+    user = User.find(id)
+    UserMailer.welcome(user).deliver
   end
 end
