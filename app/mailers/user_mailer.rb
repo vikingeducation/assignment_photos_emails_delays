85437a65ae965a4ae80272212e7e4ec45c64bc79
Roy Chen <roychenlei@gmail.com>
2017-11-30 12:29:01 +0800

    modify mailer config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-  default from: "dev@testapp.com"
+  default from: "dev@still-cove-64200.herokuapp.com"
 
   def welcome(user)
     @user = user
-    mail(to: @user.email, subject: "Welcome to TestApp!")
+    mail(to: @user.email, subject: "Welcome to still-cove-64200.herokuapp.com!")
   end
 end
