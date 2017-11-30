f394ae79082314d28a83c125e95d958dc465c303
Roy Chen <roychenlei@gmail.com>
2017-11-30 12:11:35 +0800

    set hostname for Action Mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "assignment_photos_emails_delays_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Set hostname for Action Mailer
+  config.action_mailer.default_url_options = { host: 'still-cove-64200.herokuapp.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
